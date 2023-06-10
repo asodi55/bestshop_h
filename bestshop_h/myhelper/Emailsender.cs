ef9d514830aeb1bb9aca0ef44cd70fb0c0bbcac2
asodi55 <asodisrihari1211@gmail.com>
2023-06-10 08:54:38 +0530

    Email

--- bestshop_h/myhelper/Emailsender.cs
+++ bestshop_h/myhelper/Emailsender.cs
@@ -1,37 +1,23 @@
 ﻿using SendGrid;
 using SendGrid.Helpers.Mail;
-using System.Net.Mail;
-using System.Numerics;
 
 namespace bestshop_h.myhelper
 {
     public class Emailsender
     {
-        public static object? Email { get; private set; }
-        public static object? FirstName { get; private set; }
-        public static object? LastName { get; private set; }
-        public static object? Phone { get; private set; }
-
-        public static async Task SendEmail(string toEmail, string username, string subject, string message)
+        public static void SendEmail(string toEmail, string username, string subject, string message)
         {
-            MailMessage Msg = new MailMessage();
-            Msg.From = new MailAddress("contact@winnovative.in", "TestEmail");
-            Msg.To.Add("Email");
-            Msg.CC.Add("susmithavari@gmail.com");
-            Msg.CC.Add("srihariasodi2@gmail.com");
-            Msg.Subject = "Test Email";
-            Msg.Body = "<p>Name:" + FirstName + "</p>" + "<p>Name:" + LastName + "</p>" + "<p>E-mail: " + Email + "</p>" + "<p>Phone:" + Phone + "</p>";
-            Msg.IsBodyHtml = true;
+            string apikey = ""
+                var client = new SendGridClient(apikey);
 
+            var from = new EmailAddress(username"boostmytoo@gmail.com", "BestShop.com");
+            var to = new EmailAddress(toEmail, username);
+            var plainTextContent = message;
+            var htmlContent = "";
 
+            var msg = MailHelper.CreateSingleEmail(from, to subject, plainTextContent, htmlContent);
 
-            SmtpClient smtp = new SmtpClient();
-            smtp.Host = "relay-hosting.secureserver.net";
-            smtp.Port = 25;
-            smtp.Credentials = new System.Net.NetworkCredential("contact@winnovative.in", "crossRef@040$");
-            smtp.EnableSsl = false;
-            smtp.Send(Msg);
-            Msg = null;
+            var response = client.SendEmailAsync(msg);
         }
     }
 }
